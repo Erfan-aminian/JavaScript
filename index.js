11758706ef2a73a13b4ee6da94cc9d656d2527f1
Erfan-Aminian <aminian158@gmail.com>
2025-03-14 22:35:41 +0330

    Operators2

--- index.js
+++ index.js
@@ -79,3 +79,41 @@ console.log('1'===1);
 console.log(1==1);
 console.log('1'==1);
 console.log(true==1);
+// 23
+let point = 99;
+let type = point > 100 ? "gold" : "silver";
+console.log(type)
+// 24
+// logical and &&
+// logical or ||
+// not !
+let highincome = false;
+let goodcreditscore = true;
+let eligibleforloan = highincome || goodcreditscore;
+console.log('eligible',eligibleforloan);
+let applicationrefused = !eligibleforloan
+console.log('application ',applicationrefused);
+// 25
+// falsy (false)
+// undefind
+// null
+//0
+//false
+//''
+//NaN
+let usercolor = undefined;
+let defaultcolor = 'blue';
+let currentcolor = usercolor || defaultcolor;
+console.log(currentcolor);
+//26
+const readpermission = 4;
+const writepermission = 2;
+const excutepermisson = 1;
+let mypermission = 0;
+
+mypermission =  mypermission |writepermission;
+let message = (mypermission & readpermission) ? 'yes': 'no';
+console.log(message);
+//27
+let xx = (2 +3) * 4;
+console.log(xx);
