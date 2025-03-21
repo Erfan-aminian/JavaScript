3d8544c611d0dad98b4af6234cd086ae02b5e224
Erfan-Aminian <aminian158@gmail.com>
2025-03-21 23:22:24 +0330

    Add exercise

---
+++ exercise.js
@@ -0,0 +1,5 @@
+let number = max(5, 20);
+function max (a,b){
+    return (a>b) ? a : b;
+}
+
---
+++ exercise2.js
@@ -0,0 +1,4 @@
+console.log(islandscape(300, 600));
+function islandscape(width, height){
+    return (width > height);
+}
