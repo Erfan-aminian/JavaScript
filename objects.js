afad32bff96f4dccc14b49c27206b13bffecb0da
Erfan-Aminian <aminian158@gmail.com>
2025-05-06 22:16:52 +0330

    Dynamic Constructor

--- objects.js
+++ objects.js
@@ -22,14 +22,22 @@ function createCircle(radius){
     };
 }
 const circle1 = createCircle(1);
-console.log(circle1)
+//console.log(circle1)
 // 52 Constructor Function
 
-function Circle(radius){
+function Circle55(radius){
     this.radius = radius;
     this.draw = function(){
         console.log('draw!');
     }
 }
-const circle = new Circle(1);
+//const circle = new Circle55(1);
 
+const circle3 = {
+    radius: 1,
+};
+circle3.color = 'red';
+circle3.draw = function() {}
+
+delete circle3.color;
+//console.log(circle3)
