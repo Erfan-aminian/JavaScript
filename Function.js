5f8be73fe5b4741364d7ab1c3a06f251e5095000
Erfan-Aminian <aminian158@gmail.com>
2025-06-07 23:19:40 +0330

    Add getter and setter

--- Function.js
+++ Function.js
@@ -20,50 +20,24 @@ function sum1 (discount, ...prices){
     return total * (1 - discount);
 }
 //console.log(sum1(0.1, 20, 30));
-function interest(principal, rate=3.5, year=5){
-    return principal * rate / 100 * year;
-}
-console.log(interest(10000))
-///
 
-function jame(){
-    let total = 0;
-    for (let value of arguments)
-        total += value;
-    return total;  
+function interest(principal, rate=3.5, year=5){
+    return principal * rate / 100 * year;
 }
-//console.log(sum(1, 2, 3, 4, 5));
-function minus(){
-    let total = 0;
-    for (let value of arguments)
-        total -= value;
-    return total;  
-}
-//console.log(sum(1, 2, 3, 4, 5));
+//console.log(interest(10000));
 
-function sum1 (discount, ...prices){
-    const total = prices.reduce((a, b) => a + b);
-    return total * (1 - discount);
-}
-//console.log(sum1(0.1, 20, 30));
-function interest(principal, rate=3.5, year=5){
-    return principal * rate / 100 * year;
-}
-console.log(interest(10000))
-function sum1 (discount, ...prices){
-    const total = prices.reduce((a, b) => a + b);
-    return total * (1 - discount);
-}
-//console.log(sum1(0.1, 20, 30));
-function interest(principal, rate=3.5, year=5){
-    return principal * rate / 100 * year;
-}
-console.log(interest(10000))function sum1 (discount, ...prices){
-    const total = prices.reduce((a, b) => a + b);
-    return total * (1 - discount);
-}
-//console.log(sum1(0.1, 20, 30));
-function interest(principal, rate=3.5, year=5){
-    return principal * rate / 100 * year;
-}
-console.log(interest(10000))
+const person = {
+    firstName: 'erfan',
+    lastName: 'amini',
+    get fullName() {
+        return `${person.firstName} ${person.lastName}`
+    }
+
+    set fullName(value){
+        const part = value.split(' ');
+        this.firstName = part[0];
+        this.lastName = part[1];
+    }
+};
+person.fullName = 'ali amini';
+console.log(person);
